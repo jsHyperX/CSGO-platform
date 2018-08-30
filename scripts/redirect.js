6a09dbe29608a0c0943e8167e16d29e2d7727348
jsHyperX <vikramsaurabh14@gmaiil.com>
2018-08-30 17:34:34 +0530

    more pages and redirect links added

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -2,26 +2,44 @@ function redirect2steam() {
     window.location = "https://store.steampowered.com/login/";
 }
 
-function redirect2home() {
-    window.location = "../index.html";
+function redirect2home(num) {
+    if(num) {
+        window.location = "../index.html";
+    }
+    else window.location = "./index.html";
 }
 
-function redirect2affiliates() {
-    window.location = "./dest/affiliates.html";
+function redirect2affiliates(num) {
+    if(num) {
+        window.location = "./affiliates.html";
+    }
+    else window.location = "./dest/affiliates.html";
 }
 
-function redirect2faq() {
-    window.location = "./dest/FAQ.html";
+function redirect2faq(num) {
+    if(num) {
+        window.location = "./FAQ.html";
+    }
+    else window.location = "./dest/FAQ.html";
 }
 
-function redirect2provably() {
-    window.location = "./dest/provably.html";
+function redirect2provably(num) {
+    if(num) {
+        window.location = "./provably.html";
+    }
+    else window.location = "./dest/provably.html";
 }
 
-function redirect2support() {
-    window.location = "./dest/support.html";
+function redirect2support(num) {
+    if(num) {
+        window.location = "./support.html";
+    }
+    else window.location = "./dest/support.html";
 }
 
-function redirect2tos() {
-    window.location = "./dest/tos.html";
+function redirect2tos(num) {
+    if(num) {
+        window.location = "./tos.html";
+    }
+    else window.location = "./dest/tos.html";
 }
